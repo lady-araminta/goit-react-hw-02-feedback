3d6a03bcc25d6f5ce90e283dd6deed8e1f2624d9
lady-araminta <jparkhomenko@gmail.com>
2022-12-04 12:03:35 +0200

    Fix bugs

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,62 +1,15 @@
-import { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Sect } from './Section.styled';
-import { Title } from './Title';
-import { FeedbackOptions } from './FeedbackOptions';
-import { Statistics } from './Statistics';
-import { Notification } from './Notification';
 
-export class Section extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
-
-  onUpdateStats = event => {
-    const targetName = event.currentTarget.name;
-
-    this.setState(prevState => ({ [targetName]: prevState[targetName] + 1 }));
-  };
-
-  countTotalFeedback = data => {
-    return Object.values(data).reduce((acc, total) => acc + total, 0);
-  };
-
-  countPositiveFeedbackPercentage() {
-    return Number.parseInt(
-      (this.state.good / this.countTotalFeedback(this.state)) * 100
-    );
-  }
-
-  render() {
-    const { good, neutral, bad } = this.state;
-    const totalFeedback = this.countTotalFeedback(this.state);
-    const positiveFeedbackPercentage = this.countPositiveFeedbackPercentage();
-    const message = 'There is no feedback';
-    return (
-      <Sect>
-        <Title title={this.props.title} />
-        <FeedbackOptions
-          onLeaveFeedback={this.onUpdateStats}
-          options={['good', 'neutral', 'bad']}
-        />
-        {totalFeedback > 0 ? (
-          <Statistics
-            good={good}
-            neutral={neutral}
-            bad={bad}
-            total={totalFeedback}
-            positivePercentage={positiveFeedbackPercentage}
-          />
-        ) : (
-          <Notification message={message} />
-        )}
-      </Sect>
-    );
-  }
-}
+export const Section = ({ title, children }) => {
+  return (
+    <section>
+      <h2>{title}</h2>
+      {children}
+    </section>
+  );
+};
 
 Section.propTypes = {
   title: PropTypes.string.isRequired,
+  children: PropTypes.array.isRequired,
 };
--- src/components/Title.styled.js
+++
@@ -1,3 +0,0 @@
-import styled from 'styled-components';
-
-export const TitleSt = styled.h2``;
